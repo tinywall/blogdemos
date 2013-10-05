07e97890c1df2bdab591ebd31ef77d27161db4d1
Arun David <arundavid.info@gmail.com>
2013-10-05 12:12:24 +0530

    Initial code

---
+++ footer.php
@@ -0,0 +1,8 @@
+		<div class='footer'>
+			<div class='footer-content'>
+				copyright &copy; Tinywall 2013.
+			</div>
+		</div>
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include 'header.php';?>
+<div class='main'>
+	<h1 class='title'>
+		Blog post title. Blog post title. Blog post title. Blog post title. Blog post title.
+	</h1>
+	<div class='main-header'>
+		<div class='action-links'>
+			<a href='#'>Back to Tutorial</a>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;
+			<a href='#'>Download Code</a>
+		</div>
+		<div class='top-ad'>
+			<a href='#'>Google ad. Google ad. Google ad. Google ad. Google ad. Google ad.Google ad.</a>
+		</div>
+		<div class='clear'></div>
+	</div>
+	<div class='content'>
+	<!-- *************************** content start *************************** -->
+	Blog demo content. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.<br/><br/>
+	Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
+
+
+
+
+	<!-- ***************************** content end *************************** -->
+	</div>
+	<div class='bottom-ad'>
+		Some more ads
+	</div>
+</div>
+<?php include 'footer.php';?>
